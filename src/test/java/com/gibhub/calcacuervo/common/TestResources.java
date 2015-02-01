132238438f5055b2f2afad8e4cf8602ffd99f628
Demian Calcaprina <calcacuervo@gmail.com>
2015-02-01 15:14:15 -0300

    Timer test with cron.

---
+++ src/test/java/com/gibhub/calcacuervo/common/TestResources.java
@@ -0,0 +1,16 @@
+package com.gibhub.calcacuervo.common;
+
+public enum TestResources {
+	REMINDER_WITH_INTERVAL("com/github/calcacuervo/timer/reminder_with_interval.drl"),
+	REMINDER_WITH_CRON("com/github/calcacuervo/timer/reminder_with_cron.drl");
+
+	private String resource;
+
+	private TestResources(String resource) {
+		this.resource = resource;
+	}
+
+	public String getResource() {
+		return resource;
+	}
+}
--- src/test/java/com/gibhub/calcacuervo/timer/TimerTest.java
+++ src/test/java/com/gibhub/calcacuervo/timer/TimerTest.java
@@ -11,15 +11,17 @@ import org.kie.api.runtime.conf.ClockTypeOption;
 import org.kie.api.runtime.rule.FactHandle;
 import org.kie.api.runtime.rule.QueryResults;
 
+import com.gibhub.calcacuervo.common.TestResources;
 import com.gibhub.calcacuervo.common.TestRuleUtils;
 
 public class TimerTest {
 
 	@Test
-	public void test_send_reminder() {
+	public void test_send_reminder_interval() {
 		// first, we need to create the Kie Session.
 		KieSession ksession = new TestRuleUtils().new KieSessionBuilder()
-				.withResources("com/github/calcacuervo/timer/reminder.drl")
+				.withResources(
+						TestResources.REMINDER_WITH_INTERVAL.getResource())
 				.withKieSessionOption(ClockTypeOption.get("pseudo")).build();
 
 		// now, create a customer who did the payment of the subscription.
@@ -51,6 +53,58 @@ public class TimerTest {
 
 		// so, after 15 mins
 		clock.advanceTime(15, TimeUnit.MINUTES);
+		ksession.fireAllRules();
+
+		// no new reminders have been sent.
+		// now, we may have sent 2 reminders.
+		Assert.assertEquals(2, getAmountOfReminders(ksession));
+	}
+
+	@Test
+	public void test_send_reminder_cron() {
+		// first, we need to create the Kie Session.
+		KieSession ksession = new TestRuleUtils().new KieSessionBuilder()
+				.withResources(TestResources.REMINDER_WITH_CRON.getResource())
+				.withKieSessionOption(ClockTypeOption.get("pseudo")).build();
+
+		// now, create a customer who did the payment of the subscription.
+		Customer customer = new Customer("Demian");
+		FactHandle customerFactHandle = ksession.insert(customer);
+		ksession.fireAllRules();
+		Assert.assertEquals(0, getAmountOfReminders(ksession));
+
+		// until now, no reminder has been sent.
+		// but now, we set the customer as not have done the latest payment.
+		customer.setSuscriptionPaymentDone(false);
+
+		// we have to update the ksession.
+		ksession.update(customerFactHandle, customer);
+		ksession.fireAllRules();
+
+		// now, we may have sent no reminder, as we do at the begining of the
+		// day.
+		Assert.assertEquals(0, getAmountOfReminders(ksession));
+
+		// 1 day passes, a reminder should have been sent.
+		SessionPseudoClock clock = ksession.getSessionClock();
+		clock.advanceTime(1, TimeUnit.DAYS);
+		ksession.fireAllRules();
+		// now, we may have sent 1 reminder.
+		Assert.assertEquals(1, getAmountOfReminders(ksession));
+
+		// another day passes, a reminder should have been sent.
+		clock = ksession.getSessionClock();
+		clock.advanceTime(1, TimeUnit.DAYS);
+		ksession.fireAllRules();
+		// now, we may have sent 2 reminders.
+		Assert.assertEquals(2, getAmountOfReminders(ksession));
+
+		// now, the user have done the payment.
+		customer.setSuscriptionPaymentDone(true);
+		ksession.update(customerFactHandle, customer);
+
+		// so, after another day
+		clock.advanceTime(1, TimeUnit.DAYS);
 		ksession.fireAllRules();
 
 		// no new reminders have been sent.
