8d0f7e8dc3f57e3dde63da42334ee7412cb677c9
Nico <nicolas.e.gomez@gmail.com>
2015-02-03 23:24:45 -0300

    Flight classes

--- src/test/java/com/gibhub/calcacuervo/common/TestResources.java
+++ src/test/java/com/gibhub/calcacuervo/common/TestResources.java
@@ -4,7 +4,8 @@ public enum TestResources {
 	REMINDER_WITH_INTERVAL("com/github/calcacuervo/timer/reminder_with_interval.drl"),
 	REMINDER_WITH_EXPRESSION("com/github/calcacuervo/timer/reminder_with_expression.drl"),
 	REMINDER_WITH_CALENDAR("com/github/calcacuervo/timer/reminder_with_calendar.drl"),
-	REMINDER_WITH_CRON("com/github/calcacuervo/timer/reminder_with_cron.drl");
+	REMINDER_WITH_CRON("com/github/calcacuervo/timer/reminder_with_cron.drl"),
+	FLIGHT_SIMPLE_QUERY("com/github/calcacuervo/flights/flight_simple_query.drl");
 
 	private String resource;
 
